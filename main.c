0279862760442027ef61e9652642873eca1890e2
fernandonuovi <fernando.nuovi@aluno.ifsp.edu.br>
2025-04-02 16:33:50 -0300

    main.c
    Switch Case

--- main.c
+++ main.c
@@ -2,40 +2,67 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
-int main(void) 
-{
-	// ENTRADAS
-	DDRB  &=  ~(1<<2);
-	DDRB  &=  ~(1<<1);
-	
-	// SAÍDA
-	DDRB  &= ~(1<<5); // K1
-	DDRB  &= ~(1<<4); // K2
-	DDRB  &= ~(1<<3); // K3
-	
-	// ATIVAR PULL UP
-	PORTB  |=  (1<<2);
-	PORTB  |=  (1<<1);
-	
-	// ZERANDO
-	PORTB &= ~(1<<5); 
-	PORTB &= ~(1<<4); 
-	PORTB &= ~(1<<3); 
-	
-	
-	
-    while ( 1 )
-    {
-		if( !(PINB  &= ~(1<<2))); // S1
-		{
-			PORTB |=  (1<<5); // K1
-			PORTB |=  (1<<4); // K2
-			_delay_ms(5000);
-			
-			PORTB  &=  ~(1<<4);
-			PORTB |=  (1<<3);
-		}
-		else if( !(PINB  &= ~(1<<2))); // S1
+#define S1 (1<<2) // botão de partida
+#define S0 (1<<1) // Botão de parada
+#define K1 (1<<5) // Contator K1
+#define K2 (1<<4) // Contator K2
+#define K3 (1<<3) // Contator K3
 
-    }
+int main(void)
+{
+	// Configuração botões como entrada
+	DDRB &= ~(S1 | S0);
+	
+	// Ativando Pull Up
+	PORTB |= (S1 | S0);
+	
+	// Configurando Contatores como saída
+	DDRB |= (K1 | K2 | K3);
+	
+	// Zerando as saídas
+	PORTB &= ~(K1 | K2 | K3);
+	
+	estado = 0
+	
+	switch( estado )
+	{
+		case 0:
+		PORTB &= ~(K1 | K2 | K3);
+		
+			if( !(PINB & S1) )
+			{
+				estado = 1
+			}
+		break;
+		
+		case 1:
+		PORTB |= (K1 | K2);
+		delay = 5000
+		
+		estado = 2
+		break;
+		
+		case 2: delay--;
+		_delay_ms(1);
+		
+			if( !(PINB & S0) )
+			{
+				estado = 0
+			}
+			else if( delay ==0)
+			{
+			estado = 3
+			}
+		break;
+		
+		case 3:
+		PORTB &= ~(K2);
+		PORTB |= (K3);
+		
+			if( !(PINB & S0) )
+			{
+				estado = 0
+			}
+		break;
+	}
 }
